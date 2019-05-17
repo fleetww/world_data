c73cddc9503f851e9d1a1b929ccfb62155459136
William Fleetwood <fleetww@wwu.edu>
2019-05-16 21:51:32 -0700

    Just scraping data and downloading zip files

--- scraper.py
+++ scraper.py
@@ -8,9 +8,9 @@ page = urllib.request.urlopen(country_list_url)
 if page.getcode() != 200:
     print('Failed to retrieve country list from ' + country_list_url)
 
-html = BeautifulSoup(page, 'html.parser')
+soup = BeautifulSoup(page, 'html.parser')
 
-overview = html.find('div', {'class': 'overviewArea body'})
+overview = soup.find('div', {'class': 'overviewArea body'})
 alphabet = overview.find_all('section', {'class': 'nav-item'})
 for letter in alphabet:
     countries = letter.find_all('a')
@@ -20,3 +20,11 @@ for letter in alphabet:
         country_page = urllib.request.urlopen(country_url)
         if country_page.getcode() != 200:
             print('Failed to retrieve page for ' + country.text)
+        html = BeautifulSoup(country_page, 'html.parser')
+        try:
+            link = next(x for x in html.find('div', {'class': 'btn-item download'}).find_all('a') if x.text == 'CSV')
+        except:
+            print('Could not find CSV download link for country')
+            continue
+        print(link.get('href'))
+        urllib.request.urlretrieve(link.get('href'), filename='data/' + country.text + '.zip')
