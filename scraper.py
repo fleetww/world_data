955b652dcc7c0d523931ffa8a3cd57f71757e45a
William Fleetwood <fleetww@wwu.edu>
2019-05-18 09:58:06 -0700

    Move to functions

--- scraper.py
+++ scraper.py
@@ -1,30 +1,56 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-base_url = 'https://data.worldbank.org'
-country_list_url = base_url + '/country'
-
-page = urllib.request.urlopen(country_list_url)
-if page.getcode() != 200:
-    print('Failed to retrieve country list from ' + country_list_url)
-
-soup = BeautifulSoup(page, 'html.parser')
-
-overview = soup.find('div', {'class': 'overviewArea body'})
-alphabet = overview.find_all('section', {'class': 'nav-item'})
-for letter in alphabet:
-    countries = letter.find_all('a')
-    for country in countries:
-        country_url = base_url + country.get('href')
-        print(country.text)
-        country_page = urllib.request.urlopen(country_url)
-        if country_page.getcode() != 200:
-            print('Failed to retrieve page for ' + country.text)
-        html = BeautifulSoup(country_page, 'html.parser')
+#Tries to request url twice, if it fails both it will raise the error
+def request_page(url):
+    try:
+        page = urllib.request.urlopen(url)
+    except:
         try:
-            link = next(x for x in html.find('div', {'class': 'btn-item download'}).find_all('a') if x.text == 'CSV')
+            page = urllib.request.urlopen(url)
         except:
-            print('Could not find CSV download link for country')
-            continue
-        print(link.get('href'))
-        urllib.request.urlretrieve(link.get('href'), filename='data/' + country.text + '.zip')
+            raise
+    
+    if page.getcode() != 200:
+        raise Exception('Did not receive \'200 OK\' from ' + url)
+
+    return page
+
+def scraper():
+    base_url = 'https://data.worldbank.org'
+    country_list_url = base_url + '/country'
+
+    try:
+        page = request_page(country_list_url)
+    except Exception as e:
+        print(e)
+        exit(1)
+
+    soup = BeautifulSoup(page, 'html.parser')
+    overview = soup.find('div', {'class': 'overviewArea body'})
+    alphabet = overview.find_all('section', {'class': 'nav-item'})
+    for letter in alphabet:
+        countries = letter.find_all('a')
+        for country in countries:
+            print(country.text)
+
+            country_url = base_url + country.get('href')
+            try:
+                country_page = request_page(country_url)
+            except Exception as e:
+                print(e)
+                continue
+
+            html = BeautifulSoup(country_page, 'html.parser')
+            try:
+                link = next(x for x in html.find('div', {'class': 'btn-item download'}).find_all('a') if x.text == 'CSV')
+            except:
+                print('Could not find CSV download link for ' + country.text)
+                continue
+            urllib.request.urlretrieve(link.get('href'), filename='data/' + country.text + '.zip')
+
+def main():
+    scraper()
+
+if __name__ == "__main__":
+    main()
